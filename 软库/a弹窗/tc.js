38cd8ad85508cca359c3688d498e6c335d3505d0
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-10-27 23:13:07 +0800

    Update tc.js

--- 软库/a弹窗/tc.js
+++ 软库/a弹窗/tc.js
@@ -9,5 +9,5 @@ function confirmAccess() {
 
  // 返回按钮的点击事件
  function goBack() {
-    window.location.href = '../../index.html'; // 跳转到指定页面
+    window.location.href = '../../../index.html'; // 跳转到指定页面
 }
