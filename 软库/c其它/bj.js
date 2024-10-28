e9113b5d3a8df2777be6787f8c4381b683b12f07
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-10-28 20:01:33 +0800

    Update bj.js

--- 软库/c其它/bj.js
+++ 软库/c其它/bj.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const defaultBackground = '#0d1117'; // 默认背景颜色
     const remoteBackground = 'https://vip.helloimg.com/i/2024/07/22/669e30bf7238a.jpg'; // 远程背景图片
-    const localBackground = 'c其它/背景.png'; // 本地背景图片
+    const localBackground = '背景.png'; // 本地背景图片
 
     const bodyElement = document.body;
 
