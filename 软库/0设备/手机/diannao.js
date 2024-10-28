2f2b1de88d2c7b92382932d4ea121522b11f79d7
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-10-28 21:48:06 +0800

    Update diannao.js

--- 软库/0设备/手机/diannao.js
+++ 软库/0设备/手机/diannao.js
@@ -18,13 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app); // 获取数据库实例
 
-const itemsPerPage = 1; // 每页显示 1 个软件块
-let currentPage = 1; // 当前页码，默认第一页
-let previousPage = 1; // 记录前一页的页码，初始为第一页
-let historyStack = []; // 用于记录用户操作历史
-let futureStack = []; // 用于记录用户前进操作
-let inLibraryView = false; // 标志是否在软件库查看中
-
 // 加载软件列表数据
 function loadSoftwareList() {
     const softwareListRef = ref(database, 'sites'); // 从 Firebase 数据库获取 'sites' 节点的数据
@@ -47,77 +40,10 @@ function loadSoftwareList() {
             div.innerHTML = `<span class="特效class 文字class">${item.name}</span>`; // 显示软件名称
             div.setAttribute('role', 'listitem'); // 为无障碍支持设置 role 属性
             div.setAttribute('tabindex', '0'); // 可通过键盘导航
-            div.onclick = function () {
-                loadLibrary(item.url); // 点击后加载对应软件库
-                historyStack.push({ page: currentPage, url: item.url }); // 记录操作历史，保存页面和URL
-                currentPage = item.url;
-                inLibraryView = true;
-            };
             container.appendChild(div); // 将新软件块添加到容器中
         });
     });
 }
 
-// 加载选中的软件库
-function loadLibrary(url) {
-    const container = document.getElementById('软件库大容器id');
-    container.innerHTML = ''; // 清空容器内容
-    const iframe = document.createElement('iframe'); // 创建一个内嵌框架用于展示内容
-    iframe.className = '软件库块class'; // 设置样式类
-    iframe.setAttribute('src', url); // 设置 iframe 跳转的链接
-    iframe.setAttribute('frameborder', '0'); // 设置边框为 0
-    iframe.setAttribute('width', '100%'); // 使其宽度为 100%
-    iframe.setAttribute('height', '100%'); // 高度 100%使其全屏显示 // 高度 500px
-    container.appendChild(iframe); // 将 iframe 添加到容器中
-    inLibraryView = true;
-}
-
-// 导航栏的操作函数
-function navigateBack() {
-    if (historyStack.length > 0) {
-        futureStack.push(currentPage); // 将当前页码放入前进栈
-        currentPage = historyStack.pop(); // 从历史栈中弹出页码
-        if (inLibraryView) {
-            loadLibrary(currentPage.url); // 如果在库视图中，则加载库
-        } else {
-            loadSoftwareList(); // 重新加载软件列表
-        }
-        inLibraryView = false;
-    }
-}
-
-function navigateForward() {
-    if (futureStack.length > 0) {
-        historyStack.push(currentPage); // 将当前页码放入历史栈
-        currentPage = futureStack.pop(); // 从前进栈中弹出页码
-        if (inLibraryView) {
-            loadLibrary(currentPage.url); // 如果在库视图中，则加载库
-        } else {
-            loadSoftwareList(); // 重新加载软件列表
-        }
-        inLibraryView = true;
-    }
-}
-
-function goHomeRedirect() {
-    window.location.href = '../../../../index.html'; // 跳转到指定主页
-}
-
-function goHome() {
-    currentPage = 1; // 设置页码为第一页
-    loadSoftwareList(); // 重新加载软件列表
-    inLibraryView = false;
-    historyStack = []; // 清空历史记录
-    futureStack = []; // 清空前进记录
-}
-
 // 初次加载软件列表
 loadSoftwareList();
-
-// 记录用户的操作历史
-function updateHistory() {
-    if (!inLibraryView) {
-        historyStack.push(previousPage); // 将前一页放入历史栈中
-    }
-    previousPage = currentPage; // 更新前一页的页码
-}
