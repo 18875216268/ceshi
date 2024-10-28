3e8c5071bc59df8529b5b8915c665dc0613367d1
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-10-28 22:30:08 +0800

    Update tc.js

--- 软库/a弹窗/tc.js
+++ 软库/a弹窗/tc.js
@@ -6,8 +6,3 @@ window.onload = function () {
 function confirmAccess() {
     document.getElementById('弹窗容器id').style.display = 'none'; // 隐藏弹窗
 }
-
- // 返回按钮的点击事件
- function goBack() {
-    window.location.href = '../../../../index.html'; // 跳转到指定页面
-}
