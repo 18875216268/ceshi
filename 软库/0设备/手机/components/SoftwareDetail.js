5bd51459e7e02d2380fa38c77793d8ecdf21af45
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-11-06 17:35:06 +0800

    Update SoftwareDetail.js

--- 软库/0设备/手机/components/SoftwareDetail.js
+++ 软库/0设备/手机/components/SoftwareDetail.js
@@ -3,12 +3,12 @@ import { ref, onValue } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-
 export default {
   template: `
     <div>
-      <iframe :src="softwareUrl" class="content-frame"></iframe>
+      <iframe ref="iframe" :src="softwareUrl" class="content-frame"></iframe>
     </div>
   `,
   data() {
     return {
-      softwareUrl: '',
+      softwareUrl: '', // 软件库初始 URL
     };
   },
   inject: ['db'], // 注入 Firebase 数据库实例
@@ -17,7 +17,7 @@ export default {
   },
   methods: {
     loadSoftware() {
-      const db = this.db; // 使用注入的 db 实例
+      const db = this.db;
       const softwareId = this.$route.params.id;
       const softwareRef = ref(db, `sites/${softwareId}`);
       
@@ -28,5 +28,23 @@ export default {
         }
       });
     },
+    handleIframeNavigation(event) {
+      const iframeDocument = this.$refs.iframe.contentDocument || this.$refs.iframe.contentWindow.document;
+
+      iframeDocument.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.tagName === 'A' && target.href) {
+          e.preventDefault(); // 阻止默认跳转行为
+          this.softwareUrl = target.href; // 更新 iframe 中的 URL
+          this.$refs.iframe.src = this.softwareUrl; // 在 iframe 中加载新页面
+        }
+      });
+    }
+  },
+  mounted() {
+    // 在 iframe 加载完内容后，开始监听其内部点击事件
+    this.$refs.iframe.onload = () => {
+      this.handleIframeNavigation();
+    };
   },
 };
